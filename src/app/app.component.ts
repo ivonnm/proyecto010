b5f6daa9b690226fbc21b010c1fc3015054565cb
ivonnm <66538222+ivonnm@users.noreply.github.com>
2021-06-11 20:51:14 -0500

    hola

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'proyecto010';
+  articulos = null;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.http.get("http://scratchya.com.ar/vue/datos.php").subscribe(
+      result => {
+        this.articulos == result;
+      },
+      error => {
+        console.log('problemas');
+      }
+
+    );
+  }
+}
